ccd4eea11ef8da1fd665050c55efa0b295fec449
AdilTehranli <adiljt@code.edu.az>
2023-05-02 17:15:27 +0400

    Id task crated

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Homework3;
+using Homework3.models;
+using System.Collections.Concurrent;
+
+
+Student s1 = new();
+Student s2 = new();
+Student s3 = new();
+Console.WriteLine(s1.Id);
+Console.WriteLine(s2.Id);
+Console.WriteLine(s3.Id);
---
+++ models/Student.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Homework3.models;
+
+internal class Student
+{
+    public int Id { get; set; }
+    public static int count { get; set; }
+    public Student()
+    {
+        count++;
+        Id += count;
+    }
+}
